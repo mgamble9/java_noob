953b0f1f80131e1b7b7f98a85fed4d85678b7bfb
Mike Gamble <mgamble9@yahoo.com>
2017-09-04 21:37:27 -0700

    first assignments with Java

---
+++ FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+  public static void main(String[] args) {
+    System.out.println("Hello. My name is Inigo Montoya.");
+    System.out.println("You killed my father.");
+    System.out.println("Prepare to die.");
+  }
+}
