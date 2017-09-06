0a442b4df76e8752f0482c69f50a11fd70ef17b9
Mike Gamble <mgamble9@yahoo.com>
2017-09-06 12:32:33 -0700

    Exceptions and Generics

---
+++ ExceptionGenericListsTest.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class ExceptionGenericListsTest {
+    public static void main(String[] args) {
+
+      // ArrayList<Object> myList = new ArrayList<Object>();
+      ArrayList<Integer> myList = new ArrayList<Integer>();
+      myList.add("13");
+      myList.add("hello world");
+      myList.add(48);
+      myList.add("Goodbye World");
+
+      // for (Object value: myList) {
+      //   try {
+      //   Integer castedValue = (Integer) value;
+      //   }
+      //   catch (ClassCastException e) {
+      //     System.out.println("Exception thrown: " + e + " on index: " +
+      //       value);
+      //   }
+      // }
+
+
+
+    }
+}
